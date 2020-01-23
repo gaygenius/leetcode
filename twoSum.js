cdd4a67e7159d63d0d099f5a75f6762b5d5de8ea
Jose Capo <jose@josecapo.com>
2020-01-22 20:08:46 -0800

    :wrench: Faster twoSum

--- twoSum.js
+++ twoSum.js
@@ -4,21 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  const NOT_AN_INDEX = -1;
-  const findIndex = function(nums, begin, value) {
-    for (let i = begin; i < nums.length; i++) {
-      if (nums[i] === value) {
-        return i;
-      }
+  const diffMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    const diffLookup = diffMap[nums[i]];
+    if (diffLookup >= 0) {
+      return [diffLookup, i];
     }
-    return NOT_AN_INDEX;
-  };
-
-  for (let i = 0; i < nums.length - 1; i++) {
-    let diff = target - nums[i];
-    let j = findIndex(nums, i + 1, diff);
-    if (j !== NOT_AN_INDEX) {
-      return [i, j];
-    }
+    diffMap[target - nums[i]] = i;
   }
 };
