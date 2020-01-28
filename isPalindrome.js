7934dfec9030ee4f5ee509a8228a854242031b33
Jose Capo <jose@josecapo.com>
2020-01-27 17:33:05 -0800

    :wrench: Implement isPalindrome without strings

--- isPalindrome.js
+++ isPalindrome.js
@@ -3,12 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  console.log("isPalindrome", x);
-  const s = x.toString();
-  return (
-    s
-      .split("")
-      .reverse()
-      .join("") === s
-  );
+  if (x < 0) {
+    return false;
+  }
+  let remainder = x;
+  const digits = [];
+  while (remainder > 0) {
+    const digit = remainder % 10;
+    digits.push(digit);
+    remainder = (remainder - digit) / 10;
+  }
+  const maxIndex = digits.length - 1;
+  let i = 0;
+  while (i * 2 < maxIndex) {
+    if (digits[i] !== digits[maxIndex - i]) {
+      return false;
+    }
+    i++;
+  }
+  return true;
 };
